f5ce8211ca22e9f0846c8496fb443615aeb8968b
Nick Baynham <nick.baynham@oracle.com>
2020-12-06 18:02:55 -0500

    added Get function

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
+import "errors"
+
 var store = make(map[string]string)
+var ErrorNoSuchKey = errors.New("no such key")
 
 func main() {
-	var _ = put("key", "value")
+	var _ = Put("key", "value")
 }
 
-func put(key string, value string) error {
+func Put(key string, value string) error {
 	store[key] = value
 	return nil
+}
+
+func Get (key string) (string, error) {
+	value, ok := store[key]
+	if !ok {
+		return "", ErrorNoSuchKey
+	}
+	return value, nil
 }
