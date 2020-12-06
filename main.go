cdce2d9781d4dc71fbd575b021efca4637c89a9d
Nick Baynham <nick.baynham@oracle.com>
2020-12-06 17:58:11 -0500

    basic idea

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+var store = make(map[string]string)
 
+func main() {
+	var _ = put("key", "value")
+}
+
+func put(key string, value string) error {
+	store[key] = value
+	return nil
 }
